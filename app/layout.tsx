6420738e135cf4fa20cfd7629c276396e743f37c
kei5o55 <uttk2003@gmail.com>
2026-05-24 03:18:04 +0900

    レイアウト調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          style={{ zoom: "80%",}
+        }
       >
         {children}
       </body>
