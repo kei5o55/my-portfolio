dec7ef6efb81aa37e389ec2f9cf15c00f398f773
kei5o55 <uttk2003@gmail.com>
2026-05-23 23:45:48 +0900

    色々修正
    主に文言の修正
    vercel用

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,11 @@ export default function Home() {
 
       {/* 自己紹介 */}
       <section style={{ marginBottom: 32 }}>
-        <h2>About</h2>
+        <h2 style={{ fontSize: 24 }}>About</h2>
         <p>
-          創作世界観とイラスト・Webアプリケーションを制作しています。
-          フロントエンド中心に、最近は自宅サーバ運用にも興味があります。
+          kei5ot&nbsp;or&nbsp;kei5o55&nbsp;という名前で活動しています。創作世界観とイラスト・Webアプリケーションを制作しています。<br></br>
+          <p>&nbsp;このサイトはnext.jsを使用し制作しているのですが、めちゃくちゃ便利ですね。今まで自分の作品を公開する際は、Viteのみで制作していましたが、物によってはnext.jsなどフレームワークを使用する方が圧倒的に楽だなと感じました。</p>
+          特に、worksにある作業記録ツールもviteで制作しているのですが、ルーティングの管理がファイルベースで直感的なところが、設計上絶対next.jsのほうが良いなと思いました。
         </p>
       </section>
 
@@ -46,30 +47,12 @@ export default function Home() {
             marginTop: 12,
           }}
         >
-          <h3>MISORIA : Frontier</h3>
-
-          <p style={{ opacity: 0.8 }}>
-            オリジナルADV + 探索ゲーム。React + Viteで制作中。
-          </p>
-
-          <div style={{ display: "flex", gap: 12, marginTop: 8 }}>
-            <a href="https://github.com/kei5o55/minesweeper">GitHub</a>
-          </div>
-        </div>
-
-        <div
-          style={{
-            border: "1px solid #333",
-            borderRadius: 8,
-            padding: 16,
-            marginTop: 12,
-          }}
-        >
           <h3>レジアプリ</h3>
 
           <p style={{ opacity: 0.8 }}>
             同人即売会向けレジアプリ。React + Viteで制作中。
           </p>
+          <p>最も力を入れているプロジェクトです。実運用を通して機能を改善していき、長期的に運用・育成したいと考えています。</p>
 
           <div style={{ display: "flex", gap: 12, marginTop: 8 }}>
             <a href="https://register-3k2l.vercel.app/">▶ DEMO Play</a>{/*今後indexにて実装予定*/}
@@ -110,7 +93,7 @@ export default function Home() {
           </p>
 
           <div style={{ display: "flex", gap: 12, marginTop: 8 }}>
-            <a href="https://matsudaidayo.fanbox.cc/">Fanbox →</a>
+            <a href="https://kei5ot.fanbox.cc/">Fanbox →</a>
           </div>
         </div>
       
